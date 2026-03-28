2e8d2e8e44b2d26294e70ba20838f65620533f05
kuon_msc <kuon_msc@192.168.0.4>
2026-03-29 03:38:25 +0900

    while文

---
+++ Helloworld_KatsutoNishimura/src/curriculum/b/Question3.java
@@ -0,0 +1,92 @@
+
+package curriculum.b;
+
+import java.util.Scanner;
+
+public class Question3 {
+
+	public static void main(String[] args) {
+		
+		//Q1
+		for (int i = 1; i <= 10 ; i++) {
+			System.out.println(i);
+		}
+		
+		//Q2
+		for (int i = 2; i <= 20; i++) {
+			if (i % 2 == 0) {
+				System.out.println(i);
+			}
+		}
+		
+		//Q3
+		for (int i = 10; i >= 1; i--) {
+			System.out.println(i);
+		}
+		 
+		//Q4
+		int sum = 0;
+		for (int i = 1; i <= 100; i++) {
+			sum += i;
+			
+		}
+		System.out.println(sum);
+		
+		//Q5
+		for (int i = 0; i <= 5; i++) {
+			for (int j = 0; j <= i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+		//Q6
+		int i = 1;
+		 while (i <= 10) {
+			 System.out.println(i);
+			 i++;
+		 }
+		 
+		 //Q7
+		 int count = 2;
+		  while (count <= 20) {
+				  System.out.println(count);
+				  count += 2;
+				  
+			  }
+		  
+		  //Q8
+		  
+		 i = 10;
+		 while (i >= 1) {
+			 System.out.println(i);
+			 i--;
+		 }
+	     
+		 //Q9
+		 i = 0;
+		 sum += i;
+		 while (i <= 100) {
+			 i++;
+		 }
+		 System.out.println(sum);
+		 
+		 //Q10
+		 Scanner scanner = new Scanner(System.in);
+			i = scanner.nextInt();
+			while (i == 0) {
+				System.out.println("終了しました");
+			break;
+			
+		 }
+		//Q11
+			for ( i = 1; i <= 9; i++) {
+				for ( int j=1; j <= 9; j++)
+					System.out.printf("%02d * %02d = %02d ||" , i,j,i*j);
+				System.out.println();
+			}
+	}
+	    
+	
+
+}
